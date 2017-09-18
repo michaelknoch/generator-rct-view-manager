0113e9fea6502855ee5b10a366926767eec2b75f
Michael Knoch <info@michaelknoch.de>
2017-09-18 17:01:12 +0200

    stuff

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,30 +7,45 @@ module.exports = class extends Generator {
             name: 'name',
             message: 'Your module name',
             default: this.appname, // Default to current folder name
+        }, {
+            type: 'input',
+            name: 'nativePath',
+            message: 'where to store the native part?',
+            default: 'ios',
+        }, {
+            type: 'input',
+            name: 'jsPath',
+            message: 'and where to store the js stuff?',
+            default: 'components',
         }]).then((answers) => {
             this.props = answers;
         });
     }
 
     writing() {
-        const { name } = this.props;
-        const nativePath = `ios/${name}`;
+        const { name, nativePath, jsPath } = this.props;
 
         this.fs.copyTpl(
             this.templatePath('RNDummyView.h'),
-            this.destinationPath(`${nativePath}/RN${name}.h`),
+            this.destinationPath(`${nativePath}/${name}/RN${name}.h`),
             { name },
         );
 
         this.fs.copyTpl(
             this.templatePath('RNDummyView.m'),
-            this.destinationPath(`${nativePath}/RN${name}.m`),
+            this.destinationPath(`${nativePath}/${name}/RN${name}.m`),
             { name },
         );
 
         this.fs.copyTpl(
             this.templatePath('DummyView.swift'),
             this.destinationPath(`${nativePath}/${name}.swift`),
+            { name },
+        );
+
+        this.fs.copyTpl(
+            this.templatePath('RNDummyView.js'),
+            this.destinationPath(`${jsPath}/RN${name}.js`),
             { name },
         );
     }
